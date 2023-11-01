34b888642e83c7cb180af7a0774807f318fe4d50
tylersmith <tyjsmith1@gmail.com>
2023-11-01 18:45:14 -0500

    completed lab

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(' ');
+
+    const titleCaseWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    return titleCaseWords.join(' ');
+  });
 }
